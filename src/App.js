c4a329a898ef46382eeae7d3839e7931b61192e2
slye20 <samuellim2011@gmail.com>
2024-06-26 21:56:14 +0800

    Created pages

--- src/App.js
+++ src/App.js
@@ -1,71 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import WelcomeBanner from "./Components/WelcomeBanner";
-import TaskManager from "./Components/TaskManager";
-import CommonScreen from "./Components/CommonScreen";
-import Login from "./Components/Login";
-import apiReadPosts from "./services/ReadAllPosts";
-import apiSaveTasks from "./services/SaveAllPosts";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Update tasks from the API
-function getStoredTasks(setTasks) {
-  apiReadPosts(setTasks);
-}
+import Home from "./pages/HomePage";
+import Login from "./pages/login";
+import SignUp from "./pages/SignUp";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [username, setUsername] = useState('');
-  const [userRequests, setUserRequests] = useState({});
-
-  const handleLogin = (username) => {
-    setUsername(username);
-    setIsLoggedIn(true);
-    setUserRequests((prevRequests) => ({
-      ...prevRequests,
-      [username]: {
-        outgoingRequest: { consumer: [], producer: [] },
-        inProgress: [],
-      },
-    }));
-  };
-
-  const handleLogout = () => {
-    setUsername('');
-    setIsLoggedIn(false);
-  };
-
-  useEffect(() => {
-    if (isLoggedIn) {
-      getStoredTasks(setTasks);
-    }
-  }, [isLoggedIn]);
-
-  useEffect(() => {
-    if (isLoggedIn) {
-      apiSaveTasks(tasks);
-    }
-  }, [tasks, isLoggedIn]);
-
   return (
     <Router>
-      <div>
-        {isLoggedIn ? (
-          <>
-            <WelcomeBanner
-              numTasks={tasks.length}
-              username={username}
-              onLogout={handleLogout}
-            />
-            <TaskManager tasks={tasks} setTasks={setTasks} />
-            <CommonScreen />
-          </>
-        ) : (
-          <Routes>
-            <Route path="/" element={<Login onLogin={handleLogin} />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        )}
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
       </div>
     </Router>
   );
